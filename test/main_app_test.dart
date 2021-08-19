5afefa313b2ab0d31cdaa3793e7bb34c3f22e29c
Hazhar Aziz <hazharaziz5555@gmail.com>
2021-08-19 12:56:19 +0430

    implement a simple test for main app

--- test/main_app_test.dart
+++ test/main_app_test.dart
@@ -1,10 +1,13 @@
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:saja/main.dart';
 
 void main() {
-  testWidgets('has a Text widget', (WidgetTester tester) async {
-    await tester.pumpWidget(MainApp());
+  testWidgets('has a curved navigation bar', (WidgetTester tester) async {
+    await tester.pumpWidget(
+      MainApp(),
+    );
 
-    expect(find.text('Hello World!'), findsOneWidget);
+    expect(find.byType(CurvedNavigationBar), findsOneWidget);
   });
 }
