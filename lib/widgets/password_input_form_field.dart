60bcdaeb6ed958844834767f8cbd6e3e4812aa15
Hazhar Aziz <hazharaziz5555@gmail.com>
2021-08-19 19:10:24 +0430

    implement custom button, custom text button, input widgets

---
+++ lib/widgets/password_input_form_field.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+import 'package:saja/resources/colors.dart';
+
+class PasswordInputFormField extends StatefulWidget {
+  final String? label;
+  final FormFieldValidator<String>? validator;
+  final TextEditingController? controller;
+  final ValueChanged<String>? onChanged;
+  final EdgeInsets? margin;
+  final EdgeInsets? padding;
+
+  const PasswordInputFormField({
+    Key? key,
+    @required this.label,
+    this.validator,
+    this.controller,
+    this.onChanged,
+    this.margin,
+    this.padding,
+  }) : super(key: key);
+
+  @override
+  _PasswordInputFormFieldState createState() => _PasswordInputFormFieldState();
+}
+
+class _PasswordInputFormFieldState extends State<PasswordInputFormField> {
+  bool isHidden = true;
+  Icon icon = Icon(
+    Icons.visibility_off_rounded,
+  );
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: widget.margin,
+      padding: widget.padding,
+      child: TextFormField(
+        controller: widget.controller,
+        obscureText: isHidden,
+        textDirection: TextDirection.ltr,
+        decoration: InputDecoration(
+          suffixIcon: InkWell(
+            onTap: togglePassword,
+            child: Container(child: icon),
+            focusColor: AppColors.accent(),
+          ),
+          suffixIconConstraints: BoxConstraints(minWidth: 50),
+          focusedBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.all(Radius.circular(10)),
+            borderSide: BorderSide(
+              color: AppColors.accent(),
+              width: 2,
+            ),
+          ),
+          labelText: widget.label,
+          alignLabelWithHint: true,
+          hintTextDirection: TextDirection.ltr,
+          hintStyle: TextStyle(
+            fontSize: 16,
+            fontWeight: FontWeight.w300,
+          ),
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.all(Radius.circular(10)),
+          ),
+        ),
+        onChanged: widget.onChanged,
+        validator: widget.validator,
+      ),
+    );
+  }
+
+  void togglePassword() {
+    setState(() {
+      isHidden = !isHidden;
+      if (isHidden == false) {
+        icon = Icon(
+          Icons.visibility_rounded,
+        );
+      } else {
+        icon = Icon(
+          Icons.visibility_off_rounded,
+        );
+      }
+    });
+  }
+}
