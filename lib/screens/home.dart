5f32edcd0663ad1b24a5ea68f0ce95863a7a5ef0
Hazhar Aziz <hazharaziz5555@gmail.com>
2021-08-24 22:58:35 +0430

    display estate items in the home screen

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -4,6 +4,7 @@ import 'package:saja/resources/colors.dart';
 import 'package:saja/resources/strings.dart';
 import 'package:saja/widgets/custom_button.dart';
 import 'package:saja/widgets/estate_item.dart';
+import 'package:saja/widgets/shimmers/estate_item.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -13,19 +14,9 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  final estates = List.generate(
-    30,
-    (index) => EstateItem(
-      id: 1,
-      estateType: 0,
-      delegationType: 1,
-      province: "آذربایجان غربی",
-      city: "مهاباد",
-      price: 100000,
-      imageUrl:
-          "https://thumbor.forbes.com/thumbor/960x0/https%3A%2F%2Fspecials-images.forbesimg.com%2Fimageserve%2F5f0c98c0147a4f0006753d4b%2FHouses-of-different-size-with-different-value-on-stacks-of-coins--Concept-of-%2F960x0.jpg%3Ffit%3Dscale",
-    ),
-  );
+  final _shimmerItemsCount = 5;
+  bool isLoading = false;
+  List<EstateItem> estates = [];
 
   @override
   Widget build(BuildContext context) {
@@ -53,23 +44,25 @@ class _HomeScreenState extends State<HomeScreen> {
         SliverList(
           delegate: SliverChildBuilderDelegate(
             (context, index) {
-              var estate = estates[index];
-              return EstateItemWidget(
-                estate,
-                margin: EdgeInsets.symmetric(vertical: 5, horizontal: 10),
-                padding: EdgeInsets.only(
-                  right: 12,
-                  left: 10,
-                  top: 10,
-                  bottom: 10,
-                ),
-                border: Border.all(
-                  color: AppColors.accent(),
-                  width: 1,
-                ),
-              );
+              var estate = (isLoading) ? null : estates[index];
+              return (isLoading)
+                  ? EstateItemShimmer()
+                  : EstateItemWidget(
+                      estate!,
+                      margin: EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+                      padding: EdgeInsets.only(
+                        right: 12,
+                        left: 10,
+                        top: 10,
+                        bottom: 10,
+                      ),
+                      border: Border.all(
+                        color: AppColors.accent(),
+                        width: 1,
+                      ),
+                    );
             },
-            childCount: estates.length,
+            childCount: (isLoading) ? _shimmerItemsCount : estates.length,
           ),
         )
       ],
