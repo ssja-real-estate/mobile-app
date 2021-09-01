075a1ce1ddee637eb6538c6efa57afbba90c6b5d
Hazhar Aziz <hazharaziz5555@gmail.com>
2021-09-01 23:06:35 +0430

    add message item model

---
+++ lib/models/message_item.dart
@@ -0,0 +1,13 @@
+class MessageItem {
+  int id;
+  String title;
+  bool seen;
+  String text;
+
+  MessageItem({
+    required this.id,
+    required this.title,
+    required this.seen,
+    required this.text,
+  });
+}
