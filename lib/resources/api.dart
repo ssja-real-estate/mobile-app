77e21bf09f204e4f71f986ba1d386098839aa183
unknown <rahmanzirak.rz@gmail.com>
2022-05-16 21:02:08 +0430

    creating and adding api strings

---
+++ lib/resources/api.dart
@@ -0,0 +1,16 @@
+class ApiStrings {
+  static const signupAddress = "/api/signup";
+  static const signinAddress = "/api/signin";
+  static const verifyAddress = "/api/verify";
+  static const httpSiteName = "https://www.ssja.ir:5001";
+  static const siteName = "ssja.ir:5001";
+  static const id = "id";
+  static const name = "name";
+  static const mobile = "mobile";
+  static const password = "password";
+  static const role = "role";
+  static const token = "token";
+  static const status = "status";
+  static const code = "code";
+  static const user = "user";
+}
