237c8e5335b0d19c1ae764956b65750d38569f85
unknown <rahmanzirak.rz@gmail.com>
2022-05-16 21:05:46 +0430

    creating api.dart and adding post and getverify methods

---
+++ lib/services/api/api.dart
@@ -0,0 +1,57 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import '../../resources/api.dart';
+
+class Api {
+  static Map<String, String> headeroption = {
+    "Content-Type": "application/json;charset=utf-8"
+  };
+  // todo post
+  static Future post({required String json, required String unicode}) async {
+    // unicode :
+    try {
+      print("post in api started");
+      print(ApiStrings.siteName);
+      var uri = Uri.http(ApiStrings.siteName, unicode);
+      print("json is :" + json);
+      var response = await http
+          .post(uri, body: json, headers: Api.headeroption)
+          .onError((error, stackTrace) {
+        //! when get error in posting
+        print("error");
+        print(error);
+        throw error!;
+      });
+      print(utf8.decode(response.bodyBytes));
+      print(response.statusCode);
+      var result = jsonDecode(utf8.decode(response.bodyBytes));
+      if (response.statusCode == 200) {
+        return result;
+      } else {
+        print("post in api ended2");
+        throw result['error'];
+      }
+    } catch (e) {
+      print(e);
+      throw e;
+    }
+  }
+
+  // todo verify user
+  static Future getVerify(
+      {required Map<String, dynamic> val, required String unicode}) async {
+    Uri uri = Uri.http(ApiStrings.siteName, unicode, val);
+    var response = await http.get(uri).onError((error, stackTrace) {
+      //! when get error in get
+      print("error");
+      print(error);
+      throw error!;
+    });
+    if (response.statusCode == 200) {
+      var result = jsonDecode(response.body);
+      return result;
+    } else {
+      throw response.statusCode;
+    }
+  }
+}
