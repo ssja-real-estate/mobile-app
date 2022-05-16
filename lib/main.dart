6f6e836645a2988073c8a53326d604b37b9d6173
unknown <rahmanzirak.rz@gmail.com>
2022-05-16 20:39:57 +0430

    moving app.dart to screens

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:saja/resources/colors.dart';
-import 'package:saja/screens/main_app.dart';
+import 'screens/app.dart';
 
 void main() {
+  // HttpOverrides.global = new MyHttpOverrides();
   SystemChrome.setSystemUIOverlayStyle(
     SystemUiOverlayStyle(
       statusBarColor: Colors.black,
@@ -12,40 +12,4 @@ void main() {
     ),
   );
   runApp(App());
-}
-
-class App extends StatefulWidget {
-  const App({Key? key}) : super(key: key);
-
-  @override
-  _AppState createState() => _AppState();
-}
-
-class _AppState extends State<App> {
-  @override
-  Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () {
-        FocusScope.of(context).requestFocus(new FocusNode());
-      },
-      child: MaterialApp(
-        debugShowCheckedModeBanner: false,
-        themeMode: ThemeMode.light,
-        theme: ThemeData(
-          primaryColor: AppColors.primary(),
-          colorScheme: ColorScheme.light(
-            primary: AppColors.accent(),
-          ),
-          brightness: Brightness.light,
-          fontFamily: 'IRANSans',
-        ),
-        home: SafeArea(
-          child: Directionality(
-            textDirection: TextDirection.rtl,
-            child: MainAppScreen(),
-          ),
-        ),
-      ),
-    );
-  }
 }
