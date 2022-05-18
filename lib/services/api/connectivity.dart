9dc4743e70df6becc134bd46d8d93a39926881d3
unknown <rahmanzirak.rz@gmail.com>
2022-05-18 15:20:18 +0430

    add connectivity

---
+++ lib/services/api/connectivity.dart
@@ -0,0 +1,15 @@
+import 'package:connectivity_plus/connectivity_plus.dart';
+
+class CheckInternet {
+  static Future hasInternet() async {
+    var connectivityResult = await (Connectivity().checkConnectivity());
+    if (connectivityResult == ConnectivityResult.mobile ||
+        connectivityResult == ConnectivityResult.wifi ||
+        connectivityResult == ConnectivityResult.ethernet) {
+      return true;
+    }
+    return false;
+  }
+
+  static Future usingVpn() async {}
+}
