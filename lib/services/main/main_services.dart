961675771eed69ace93e233409555c0736e33006
unknown <rahmanzirak.rz@gmail.com>
2022-05-21 18:49:52 +0430

    nothing

--- lib/services/main/main_services.dart
+++ lib/services/main/main_services.dart
@@ -2,29 +2,29 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:saja/services/database/hive_services.dart';
 
-class MainServices{
-  
-static Future<void> initializing() async {
-  widgetBindingInitializing();
-  await databaseInitializing();
-  systemChromeInitializing();
-}
-static Future<void> databaseInitializing() async {
-  await HiveServices.loginInitializing(
-      await HiveServices.databaseInitializing());
-}
+class MainServices {
+  static Future<void> initializing() async {
+    widgetBindingInitializing();
+    await databaseInitializing();
+    systemChromeInitializing();
+  }
 
-static void widgetBindingInitializing() {
-  WidgetsFlutterBinding.ensureInitialized();
-}
+  static Future<void> databaseInitializing() async {
+    await HiveServices.loginInitializing(
+        await HiveServices.databaseInitializing());
+  }
 
-static void systemChromeInitializing() {
-  SystemChrome.setSystemUIOverlayStyle(
-    SystemUiOverlayStyle(
-      statusBarColor: Colors.black,
-      statusBarBrightness: Brightness.light,
-      statusBarIconBrightness: Brightness.light,
-    ),
-  );
-}
+  static void widgetBindingInitializing() {
+    WidgetsFlutterBinding.ensureInitialized();
+  }
+
+  static void systemChromeInitializing() {
+    SystemChrome.setSystemUIOverlayStyle(
+      SystemUiOverlayStyle(
+        statusBarColor: Colors.black,
+        statusBarBrightness: Brightness.light,
+        statusBarIconBrightness: Brightness.light,
+      ),
+    );
+  }
 }
