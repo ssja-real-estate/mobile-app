21be6ac0cc6b4fea14ef28416ea60d26a4130f71
unknown <rahmanzirak.rz@gmail.com>
2022-05-26 15:01:21 +0430

    nothing

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,11 @@ import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:hive/hive.dart';
 import 'package:saja/services/api/certificate.dart';
 import 'screens/app.dart';
 
-void main() async{
-  HttpOverrides.global =  MyHttpOverrides();
+void main() async {
+  HttpOverrides.global = MyHttpOverrides();
   WidgetsFlutterBinding.ensureInitialized();
   SystemChrome.setSystemUIOverlayStyle(
     SystemUiOverlayStyle(
