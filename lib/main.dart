5f56c876c0883da56b4b071f9b6ef2dd9794e7c8
unknown <rahmanzirak.rz@gmail.com>
2022-05-26 11:11:22 +0430

    add cetification true

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:hive/hive.dart';
+import 'package:saja/services/api/certificate.dart';
 import 'screens/app.dart';
 
 void main() async{
-  // HttpOverrides.global = new MyHttpOverrides();
+  HttpOverrides.global =  MyHttpOverrides();
   WidgetsFlutterBinding.ensureInitialized();
   SystemChrome.setSystemUIOverlayStyle(
     SystemUiOverlayStyle(
