a6492e08d4a8f2e1c03515059d85beb123c832b1
unknown <rahmanzirak.rz@gmail.com>
2022-06-08 18:56:59 +0430

    add some estate string

--- lib/resources/Estate.dart
+++ lib/resources/Estate.dart
@@ -1,6 +1,15 @@
 class EstateStrings {
- static  const assignmentId="id";
- static  const assignmentName="name";
- static  const estateTypeId="id";
- static  const estateTypeName="name";
+  static const assignmentId = "id";
+  static const assignmentName = "name";
+  static const estateTypeId = "id";
+  static const estateTypeName = "name";
+  static const provinceId = "id";
+  static const provinceName = "name";
+ static const cityId = "id";
+  static const cityName = "name";
+  static const provinceCities = "cities";
+  static const neighborhoods = "neighborhoods";
+  static const noCityError="شهر مورد نظر یافت نشد";
+
+
 }
